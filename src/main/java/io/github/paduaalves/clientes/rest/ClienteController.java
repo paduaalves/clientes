0a3fd75724bf2d9365dbc99cfb4c2496bc9f3d43
Pádua Alves <paduaalves03@gmail.com>
2021-03-22 11:38:51 -0300

    Criando o controller de cliente

---
+++ src/main/java/io/github/paduaalves/clientes/rest/ClienteController.java
@@ -0,0 +1,28 @@
+package io.github.paduaalves.clientes.rest;
+
+import io.github.paduaalves.clientes.model.entity.Cliente;
+import io.github.paduaalves.clientes.model.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/clientes")
+public class ClienteController {
+
+    private final ClienteRepository repository;
+
+    @Autowired
+    public ClienteController(ClienteRepository repository) {
+        this.repository = repository;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente salvar(Cliente cliente){
+        return repository.save(cliente);
+    }
+}
