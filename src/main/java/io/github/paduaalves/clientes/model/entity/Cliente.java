27d3ffd61cb1ff0e7e75b4a21f6eb001db42549a
Pádua Alves <paduaalves03@gmail.com>
2021-03-22 01:58:27 -0300

    Criando e mapeando a classe Cliente

---
+++ src/main/java/io/github/paduaalves/clientes/model/entity/Cliente.java
@@ -0,0 +1,22 @@
+package io.github.paduaalves.clientes.model.entity;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Data
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(nullable = false, length = 150)
+    private String nome;
+    @Column(nullable = false, length = 11)
+    private String cpf;
+    private LocalDate datacCadastro;
+
+}
