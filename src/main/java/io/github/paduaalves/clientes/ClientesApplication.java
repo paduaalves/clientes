907532efbdd2e242d387ec5f100c278172408313
Pádua Alves <paduaalves03@gmail.com>
2021-03-22 11:38:24 -0300

    Testando conexão com o banco de dados

--- src/main/java/io/github/paduaalves/clientes/ClientesApplication.java
+++ src/main/java/io/github/paduaalves/clientes/ClientesApplication.java
@@ -1,10 +1,24 @@
 package io.github.paduaalves.clientes;
 
+import io.github.paduaalves.clientes.model.entity.Cliente;
+import io.github.paduaalves.clientes.model.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ClientesApplication {
+
+    @Bean
+    public CommandLineRunner run(@Autowired ClienteRepository repository){
+        return args -> {
+            Cliente cliente = Cliente.builder().cpf("00000000000").nome("Fulano").build();
+            repository.save(cliente);
+        };
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(ClientesApplication.class, args);
     }
