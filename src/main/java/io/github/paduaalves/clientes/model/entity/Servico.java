0ae557afc4518cb773e68fd3213792811d3a4fc9
Pádua Alves <paduaalves03@gmail.com>
2021-03-22 02:11:17 -0300

    Ajustando a identacao das classes

--- src/main/java/io/github/paduaalves/clientes/model/entity/Servico.java
+++ src/main/java/io/github/paduaalves/clientes/model/entity/Servico.java
@@ -9,14 +9,18 @@ import java.time.LocalDate;
 @Entity
 @Data
 public class Servico {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     @Column(nullable = false, length = 150)
     private String descricao;
+
     @ManyToOne
     @JoinColumn(name = "id_cliente")
     private Cliente cliente;
+
     @Column
     private BigDecimal valor;
 }
